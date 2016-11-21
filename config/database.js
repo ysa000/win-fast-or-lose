589f93b8548725e2ed90572c790a678e8945bbaa
Ysa <isabelle.yim@gmail.com>
2016-11-21 22:24:44 +0100

    update mongodb uri for production

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'url': 'mongodb://localhost:27017/express-quiz'
-	// 'url': 'mongodb://heroku_b7btlhcj:6pludh064klu2eot2cr17md242@ds159517.mlab.com:59517/heroku_b7btlhcj'
+	// 'url': 'mongodb://localhost:27017/express-quiz'
+	'url': 'mongodb://heroku_b7btlhcj:6pludh064klu2eot2cr17md242@ds159517.mlab.com:59517/heroku_b7btlhcj'
 };
